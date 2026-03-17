7d254338479fcfeb2e9b6f4946068e2f6741789a
luhanxin <154898266+juwenzhang@users.noreply.github.com>
2026-03-17 22:27:57 +0800

    Change base URL in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ux_platform/',
+  base: '/',
   plugins: [react()],
   build: {
     // 代码分割策略
